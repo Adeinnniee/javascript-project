ea93dbb16ce9ca177f7be314b59bb81738e412aa
Adeinnniee <101954385+Adeinnniee@users.noreply.github.com>
2022-06-24 17:22:47 -0700

    Add files via upload

---
+++ About-me.js
@@ -0,0 +1,7 @@
+// creating variables
+let my_name  = "Inioluwa Adeye";
+let my_height = 55;
+let my_country = "Nigeria";
+// this prints out my name, height and country
+console.log("My name is " + my_name + ". I am " + my_height + "cm in height. " + my_country +" is my beloved country.");
+
